6417eb78e231630c35767c68fb6a30adfb6674b2
GitHub Action <action@github.com>
2026-09-04 18:08:47 +0530

    feat: update evidence links to sample evidence document and dynamically read institutions data

--- routes/institutions.js
+++ routes/institutions.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const router = express.Router();
-const institutions = require("../data/institutions.json");
+
+const dataPath = path.join(__dirname, "../data/institutions.json");
+
+function getInstitutions() {
+  try {
+    const rawData = fs.readFileSync(dataPath, "utf8");
+    return JSON.parse(rawData);
+  } catch (err) {
+    return [];
+  }
+}
 
 /**
  * GET /api/institutions
  * Fetch all institution records
  */
 router.get("/", (req, res) => {
+  const institutions = getInstitutions();
   res.json({
     success: true,
     count: institutions.length,
@@ -20,6 +33,7 @@ router.get("/", (req, res) => {
  */
 router.get("/:id", (req, res) => {
   const id = Number(req.params.id);
+  const institutions = getInstitutions();
   const institution = institutions.find((item) => item.id === id);
 
   if (!institution) {
